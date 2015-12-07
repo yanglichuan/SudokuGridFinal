6839790ae0bd114a4cd93355196a69649fdab640
yanglichuan <yanglichuancom@163.com>
2015-12-07 16:45:32 +0800

    手势解锁final

---
+++ 04-手势解锁/HMLockView.h
@@ -0,0 +1,13 @@
+//
+//  HMLodeView.h
+//  04-手势解锁
+//
+//  Created by apple on 14-9-4.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMLockView : UIView
+
+@end
---
+++ 04-手势解锁/HMViewController.h
@@ -0,0 +1,13 @@
+//
+//  HMViewController.h
+//  04-手势解锁
+//
+//  Created by apple on 14-9-4.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HMViewController : UIViewController
+
+@end
